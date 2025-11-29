0878a9f25b534e8137c7e1f7aaf76603d26b54cc
dineshctoula <ds9806026041@gmail.com>
2025-11-29 09:29:08 +0545

    python data types and operations

---
+++ 2. python datatypes and operators.py
@@ -0,0 +1,103 @@
+# lists are mutable it means we can modify during the compile time 
+
+
+# dictionary are always key and value pair 
+
+
+
+age1=25
+age2=30
+age3=40
+avg=(age1+age2+age3)/3
+print(avg)
+
+
+a = 100 // 5  # how many times 5 fits in 100
+print(a)
+
+
+
+
+# power of 5 to 5 
+
+a=5**5
+print(a)
+
+
+
+gross=20
+salary=gross*20
+print(salary)
+
+
+a=90%4  #module print remainder of any division 
+
+print(a)
+
+
+# datatype examples 
+
+x=20
+print(type(x))
+
+x=2.5
+print(type(x))
+
+x='true'
+print(type(x))
+
+x=True
+print(type(x))
+
+
+x=('1','2') # tuple sano bracket ma hunxa 
+print(type(x))
+
+
+x=['1','2']  #list ho yo
+print(type(x))
+
+
+x={'name':'python',  'type':'class'}     #dictionnary ho
+print(type(x))
+
+
+
+x={"apple", "banana", "charry"}         #set ho 
+print(type(x))
+
+
+x=3+4j
+print(type(x))
+
+
+x=range(2,7)
+print(type(x))
+print(x)     #loop  bata range ko lai access garna sakinxa 
+
+
+
+x=True
+print(type(x))
+
+
+x= b" this is a python class"             #b bhaneko bytes type ho string types of bytes
+print(type(x))
+
+x=bytes([72,101,108,108,111])           #ascii key bata bolako ho so output hello world aauxa 
+print(type(x))
+print(x)
+
+
+
+
+empty=bytes(5) #this is empty bites
+print(empty)
+
+
+
+data_types= b"hello"
+
+
+
+
